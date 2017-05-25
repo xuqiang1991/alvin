bd6f779b3c9aa8d2eaf2762794bb7c39ef08da0c
522957299@qq.com <xuqiang123>
2017-05-25 18:07:39 +0800

    init 打包，定时任务并行

--- alvin-demo/src/main/java/com/alvin/demo/Application.java
+++ alvin-demo/src/main/java/com/alvin/demo/Application.java
@@ -4,14 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan(basePackages = "com.alvin")
 @MapperScan(basePackages = "com.alvin.demo.mapper")
-@PropertySource("classpath:datasource.properties")
 public class Application {
 
 	public static void main(String[] args) {
--- alvin-demo/src/main/java/com/alvin/demo/config/DruidDBConfig.java
+++ alvin-demo/src/main/java/com/alvin/demo/config/DruidDBConfig.java
@@ -7,16 +7,14 @@ import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.autoproxy.BeanNameAutoProxyCreator;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,91 +26,11 @@ import java.util.Map;
 public class DruidDBConfig {
     private Logger logger = LoggerFactory.getLogger(DruidDBConfig.class);
 
-    @Value("${druid.datasource.url}")
-    private String dbUrl;
-
-    @Value("${druid.datasource.username}")
-    private String username;
-
-    @Value("${druid.datasource.password}")
-    private String password;
-
-    @Value("${druid.datasource.driverClassName}")
-    private String driverClassName;
-
-    @Value("${druid.datasource.initialSize}")
-    private int initialSize;
-
-    @Value("${druid.datasource.minIdle}")
-    private int minIdle;
-
-    @Value("${druid.datasource.maxActive}")
-    private int maxActive;
-
-    @Value("${druid.datasource.maxWait}")
-    private int maxWait;
-
-    @Value("${druid.datasource.timeBetweenEvictionRunsMillis}")
-    private int timeBetweenEvictionRunsMillis;
-
-    @Value("${druid.datasource.minEvictableIdleTimeMillis}")
-    private int minEvictableIdleTimeMillis;
-
-    @Value("${druid.datasource.validationQuery}")
-    private String validationQuery;
-
-    @Value("${druid.datasource.testWhileIdle}")
-    private boolean testWhileIdle;
-
-    @Value("${druid.datasource.testOnBorrow}")
-    private boolean testOnBorrow;
-
-    @Value("${druid.datasource.testOnReturn}")
-    private boolean testOnReturn;
-
-    @Value("${druid.datasource.poolPreparedStatements}")
-    private boolean poolPreparedStatements;
-
-    @Value("${druid.datasource.maxPoolPreparedStatementPerConnectionSize}")
-    private int maxPoolPreparedStatementPerConnectionSize;
-
-    @Value("${druid.datasource.filters}")
-    private String filters;
-
-    @Value("${druid.datasource.connectionProperties}")
-    private String connectionProperties;
 
     @Bean     //声明其为Bean实例
-    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
+    @ConfigurationProperties(prefix="druid.datasource")
     public DataSource dataSource(){
-        DruidDataSource datasource = new DruidDataSource();
-
-        datasource.setUrl(this.dbUrl);
-        datasource.setUsername(username);
-        datasource.setPassword(password);
-        datasource.setDriverClassName(driverClassName);
-
-        //configuration
-        datasource.setInitialSize(initialSize);
-        datasource.setMinIdle(minIdle);
-        datasource.setMaxActive(maxActive);
-        datasource.setMaxWait(maxWait);
-        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-        datasource.setValidationQuery(validationQuery);
-        datasource.setTestWhileIdle(testWhileIdle);
-        datasource.setTestOnBorrow(testOnBorrow);
-        datasource.setTestOnReturn(testOnReturn);
-        datasource.setPoolPreparedStatements(poolPreparedStatements);
-        datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
-        try {
-            datasource.setFilters(filters);
-        } catch (SQLException e) {
-            logger.error("druid configuration initialization filter", e);
-        }
-        datasource.setConnectionProperties(connectionProperties);
-
-        return datasource;
+        return new DruidDataSource();
     }
 
 
